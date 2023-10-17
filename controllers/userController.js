89827160493301e5b36c949fa5df903917a5463b
Maya N <nettathm@gmail.com>
2023-10-17 17:13:33 -0600

    tested userRoutes in Insomnia, 2 GET routes and post work properly

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,28 @@ module.exports = {
   async getUsers(req, res) {
     try {
       const users = await User.find();
-      res.json("You have reached the get all users route!");
+      res.json(users);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // GET a single user by its _id and populated through and friend data
+  async getSingleUser(req, res) {
+    try {
+      const userSelected = await User.findOne({ _id: req.params.userId });
+      if (!userSelected) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(userSelected);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  // POST a new user
+  async createUser(req, res) {
+    try {
+      const newUserData = await User.create(req.body);
+      res.json(newUserData);
     } catch (error) {
       res.status(500).json(error);
     }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
-const { getUsers } = require("../../controllers/userController");
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+} = require("../../controllers/userController");
 
 // http://localhost:3001/api/users/
-router.route("/").get(getUsers);
+router.route("/").get(getUsers).post(createUser);
+
+// http://localhost:3001/api/users/:userId
+router.route("/:userId").get(getSingleUser);
 
 module.exports = router;
